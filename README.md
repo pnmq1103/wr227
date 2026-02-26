3772cf6ce551a18203f8a7e2c6a859dcb85ee8e0
Vòng Chí Văn <48863061+ChiVan222@users.noreply.github.com>
2026-02-26 21:57:31 +0700

    add todo list

--- README.md
+++ README.md
@@ -10,3 +10,9 @@
 - [x] [PA1](https://docs.google.com/document/d/1EMJ5UtmZ4s529wdfB6jzrk_XtRUov0Cv9JmKBHgcT_I/)
 - [ ] PA2
 - [ ] PA3
+
+## TODO LIST: 
+- [] Implement Transformer architecture
+- [] Design a general attention model 
+- [] Implement ViT with different kind of attention mechanisms
+- [] Searching for a bench mark and performing analysis with the result of implemented ViT models.
--- Transformer/model.py
+++ Transformer/model.py
@@ -16,9 +16,9 @@ class PositionalEncoding(nn.Module):
          self.seq_len = seq_len
          self.drop_out = nn.Dropout(drop_out)
          
-         self.pe = torch.zeros(seq_len-1,d_model)
+         self.pe = torch.zeros(seq_len,d_model)
 
-         position = torch.arange(0,seq_len-1, dtype= torch.float).unsqueeze(1)
+         position = torch.arange(0,seq_len, dtype= torch.float).unsqueeze(1)
          div_term = torch.exp(torch.arange(0,d_model,2).float()*(-math.log(10000.0)/d_model)) 
          self.pe[:,0::2] =  torch.sin(position*div_term)
          self.pe[:,1::2] =  torch.cos(position*div_term)
