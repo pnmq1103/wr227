b52dd7c2f163e9d5a2228241ab70d071ee272bba
pnmq <minhquan8046@gmail.com>
2026-03-03 23:56:36 +0700

    add introduction, related work, citations

--- README.md
+++ README.md
@@ -7,12 +7,16 @@
 ## Project Description
 
 - Topic: Comparative Survey on Attention Mechanisms
-- [x] [PA1](https://docs.google.com/document/d/1EMJ5UtmZ4s529wdfB6jzrk_XtRUov0Cv9JmKBHgcT_I/)
-- [ ] PA2
-- [ ] PA3
+- [x] [PA1](https://docs.google.com/document/d/1EMJ5UtmZ4s529wdfB6jzrk_XtRUov0Cv9JmKBHgcT_I/): Topic Proposal
+- [x] PA2: Introduction, Related Work
+- [ ] PA3: Method
+- [ ] PA4: Experiment and Results
+- [ ] PA5: Conclusion and Abstract
 
-## TODO LIST: 
+## TODO LIST:
+
 - [ ] Implement Transformer architecture
-- [ ] Design a general attention model 
-- [ ] Implement ViT with different kind of attention mechanisms
-- [ ] Searching for a bench mark and performing analysis with the result of implemented ViT models. 
+- [ ] Implement ViT with different kind of linear attention mechanisms
+- [ ] Benchmark and analyze performance implemented ViT models.
+
+- [Literature Review](https://docs.google.com/spreadsheets/d/13qwilvCUy6kqlFihILLLDaHHqWTQjU1UKLHDgjB9DzE/)
--- Transformer/model.py
+++ Transformer/model.py
@@ -1,31 +1,42 @@
+import math
+
 import torch
 import torch.nn as nn
-import math
+
+
 class InputEmbedding(nn.Module):
-    def __init__(self, d_model:int, vocab_size:int)->None:
-        super().__init__() 
+    def __init__(self, d_model: int, vocab_size: int) -> None:
+        super().__init__()
         self.d_model = d_model
         self.vocab_size = vocab_size
-        self.embedding = nn.Embedding(vocab_size,d_model)
-    def forward(self,x):  
-        return self.embedding(x)*math.sqrt(self.d_model)
-class PositionalEncoding(nn.Module): 
-    def __init__(self, d_model :int , seq_len: int, drop_out: float)->None: 
-         super().__init__()
-         self.d_model =  d_model
-         self.seq_len = seq_len
-         self.drop_out = nn.Dropout(drop_out)
-         
-         pe = torch.zeros(seq_len,d_model)
+        self.embedding = nn.Embedding(vocab_size, d_model)
 
-         position = torch.arange(0,seq_len, dtype= torch.float).unsqueeze(1)
-         div_term = torch.exp(torch.arange(0,d_model,2).float()*(-math.log(10000.0)/d_model)) 
-         pe[:,0::2] =  torch.sin(position*div_term)
-         pe[:,1::2] =  torch.cos(position*div_term)
-         pe =  pe.squeeze(0)
-def main(): 
-    pa = PositionalEncoding(512,10,0.1)
+    def forward(self, x):
+        return self.embedding(x) * math.sqrt(self.d_model)
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, seq_len: int, drop_out: float) -> None:
+        super().__init__()
+        self.d_model = d_model
+        self.seq_len = seq_len
+        self.drop_out = nn.Dropout(drop_out)
+
+        pe = torch.zeros(seq_len, d_model)
+
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.squeeze(0)
+
+
+def main():
+    pa = PositionalEncoding(512, 10, 0.1)
     print(pa.pe.shape)
+
+
 if __name__ == "__main__":
     main()
-
