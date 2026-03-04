58d167d678c38a48f6d6c036035fc855a3f4bace
Vòng Chí Văn <48863061+ChiVan222@users.noreply.github.com>
2026-03-05 00:02:02 +0700

    build transformer architecture
    Done the architecture and training part

---
+++ transformer/config.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+def get_config():
+    return{
+        "batch_size": 16,
+        "num_epochs":20,
+        "lr": 10e-4, 
+        "seq_len": 350,
+        "d_model": 512,
+        "lang_src":"en",
+        "lang_tar":"it",
+        "model_folder":"weights",
+        "model_filename":"tmodels_",
+        "preload": None,
+        "tokenizer_file": "tokenizer_{0}.json",
+        "experiment_name": "runs/tmodel"
+    }
+def get_weights_file_path(config, epoch:str):
+    model_folder = config['model_folder']
+    model_basename = config['model_basename']
+    model_filename = f"{model_basename}{epoch}.pt"
+    return str(Path('.')/model_folder/model_filename)
