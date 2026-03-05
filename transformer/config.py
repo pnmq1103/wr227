7006d5f0db803f710a81bc08ba79e3a7f02827e1
pnmq <minhquan8046@gmail.com>
2026-03-05 09:50:06 +0700

    remove redundant folder

--- transformer/config.py
+++ transformer/config.py
@@ -1,21 +1,26 @@
 from pathlib import Path
+
+
 def get_config():
-    return{
+    return {
         "batch_size": 16,
-        "num_epochs":20,
-        "lr": 10e-4, 
+        "num_epochs": 20,
+        "lr": 10e-4,
         "seq_len": 350,
         "d_model": 512,
-        "lang_src":"en",
-        "lang_tar":"it",
-        "model_folder":"weights",
-        "model_filename":"tmodels_",
+        "lang_src": "en",
+        "lang_tar": "it",
+        "model_folder": "weights",
+        "model_filename": "tmodels_",
         "preload": None,
         "tokenizer_file": "tokenizer_{0}.json",
-        "experiment_name": "runs/tmodel"
+        "experiment_name": "runs/tmodel",
     }
-def get_weights_file_path(config, epoch:str):
-    model_folder = config['model_folder']
-    model_basename = config['model_basename']
+
+
+def get_weights_file_path(config, epoch: str):
+    model_folder = config["model_folder"]
+    model_basename = config["model_basename"]
     model_filename = f"{model_basename}{epoch}.pt"
-    return str(Path('.')/model_folder/model_filename)
+    return str(Path(".") / model_folder / model_filename)
+
